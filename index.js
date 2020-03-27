38af9746a7ac0ba609f8ea5986e76019f97b456a
Ricardo Romero <ricardoromero4444@gmail.com>
2020-03-26 22:42:00 -0300

    Added notes

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const server = express();
 
+// Query params -> ?id=1 -> req.query.id
+// Route params -> /users/:id -> req.params.id
+// Request body -> { "name": "Ricardo", "email": ricardoromero4444@gmail.com }
+// req.body
+
 server.get('/test', (req, res) => {
-  return res.json({message: 'Hello world'});
-})
+  return res.json({ message: 'Hello world' });
+});
 
 server.listen(3000);
