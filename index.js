30420a104ba6ac10a5f2d291795da4057034be8c
Ricardo Romero <ricardoromero4444@gmail.com>
2020-03-26 22:48:57 -0300

    Added nodemon

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const server = express();
-
+const port = 3000;
 // Query params -> ?id=1 -> req.query.id
 // Route params -> /users/:id -> req.params.id
 // Request body -> { "name": "Ricardo", "email": ricardoromero4444@gmail.com }
@@ -11,4 +11,5 @@ server.get('/test', (req, res) => {
   return res.json({ message: 'Hello world' });
 });
 
-server.listen(3000);
+console.log('Listening on localhost:' + port)
+server.listen(port);
